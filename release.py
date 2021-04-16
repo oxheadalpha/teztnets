30f0676429bd0a2aaaa0f62fd8f0753baaa60a01
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-04-16 13:42:26 -0700

    templatize markdown, release notes

--- release.py
+++ release.py
@@ -6,6 +6,7 @@ import jinja2
 
 os.makedirs("target/release", exist_ok=True)
 
+teztnets = {}
 for network in [ f.path for f in os.scandir(".") if f.is_dir() and f.path[:3] != "./." and f.path != "./node_modules" and f.path != "./target" and f.path != "./src" ]:
     with open(f"{network}/values.yaml", 'r') as stream:
         network_values = yaml.safe_load(stream)
@@ -29,7 +30,12 @@ for network in [ f.path for f in os.scandir(".") if f.is_dir() and f.path[:3] !=
 
     with open(f"target/release/{network_name}", "w") as out_file:
         print(json.dumps(network_config), file=out_file)
+    teztnets[network_name] = { "chain_name": node_config_network["chain_name"],
+            "network_url": f"https://tqtezos.github.io/teztnets/{network_name}",
+            "command": network_values["protocol"]["command"] }
 
-index = jinja2.Template(open('src/release_notes.md.jinja2').read()).render(network="mondaynet")
-with open("target/release/index.html", "w") as out_file:
+index = jinja2.Template(open('src/release_notes.md.jinja2').read()).render(teztnets=teztnets)
+with open("target/release/index.md", "w") as out_file:
     print(index, file=out_file)
+with open("target/release/teztnets.json", "w") as out_file:
+    print(json.dumps(teztnets), file=out_file)
