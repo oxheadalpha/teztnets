f6d526da11487347969f07d224571467a4b99a05
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-05-02 11:13:24 -0700

    mondaynet readme fixes

--- index.ts
+++ index.ts
@@ -162,7 +162,7 @@ const mondaynet_chain = new TezosChain(
     category: periodicCategory,
     humanName: "Mondaynet",
     description:
-      "A testnet that restarts every Monday launched from tezos/tezos master branch. It runs ithaca for 8 cycles then upgrades to proto Alpha.",
+      "A testnet that restarts every Monday launched from tezos/tezos master branch. It runs jakarta for 8 cycles then upgrades to proto Alpha.",
     schedule: "0 0 * * MON",
     bootstrapPeers: [
       "mondaynet.ecadinfra.com",
