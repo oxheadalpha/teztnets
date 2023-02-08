678c518cb8ca4ee0c5eda094b28140c285499043
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-02-07 17:23:56 -0800

    fix mumbai descr

--- index.ts
+++ index.ts
@@ -166,7 +166,7 @@ const mondaynet_chain = new TezosChain(
     category: periodicCategory,
     humanName: "Mondaynet",
     description:
-      "A testnet that restarts every Monday launched from tezos/tezos master branch. It runs Lima with SCORU and ZK feature flags on for 8 cycles then upgrades to proto Alpha.",
+      "A testnet that restarts every Monday launched from tezos/tezos master branch. It runs Mumbai for 8 cycles then upgrades to proto Alpha.",
     schedule: "0 0 * * MON",
     bootstrapPeers: [
       "mondaynet.ecadinfra.com",
