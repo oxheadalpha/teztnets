2d1a96800fc209f09b406d611a52493e358c57fb
Nicolas Ochem <nicolas.ochem@gmail.com>
2023-08-18 16:46:09 -0700

    update validation script

--- scripts/mainnet_param_differ.py
+++ scripts/mainnet_param_differ.py
@@ -9,16 +9,16 @@ def flatten_params(params):
         params["dal_" + p]=dal_params[p]
     return params
 
-with open("nairobinet/values.yaml", "r") as f:
+with open("oxfordnet/values.yaml", "r") as f:
     params = flatten_params(yaml.safe_load(f)["activation"]["protocol_parameters"])
 
 mainnet_params = flatten_params(requests.get("https://mainnet.oxheadhosted.com/chains/main/blocks/head/context/constants").json())
 
 ghostnet_params = flatten_params(requests.get("https://rpc.ghostnet.teztnets.xyz/chains/main/blocks/head/context/constants").json())
 
-mumbainet_params = flatten_params(requests.get("https://rpc.mumbainet.teztnets.xyz/chains/main/blocks/head/context/constants").json())
+nairobinet_params = flatten_params(requests.get("https://rpc.nairobinet.teztnets.xyz/chains/main/blocks/head/context/constants").json())
 
-print("Param,mumbainet,limanet,ghostnet,mainnet")
+print("Param,oxfordnet,nairobinet,ghostnet,mainnet")
 for param in params.keys():
-    if not (params[param] == mainnet_params.get(param) == ghostnet_params.get(param) == mumbainet_params.get(param)):
-        print(f"{param},{params[param]},{mumbainet_params.get(param, 'not defined')},{ghostnet_params.get(param, 'not defined')},{mainnet_params.get(param, 'not defined')}")
+    if not (params[param] == mainnet_params.get(param) == ghostnet_params.get(param) == nairobinet_params.get(param)):
+        print(f"{param},{params[param]},{nairobinet_params.get(param, 'not defined')},{ghostnet_params.get(param, 'not defined')},{mainnet_params.get(param, 'not defined')}")
